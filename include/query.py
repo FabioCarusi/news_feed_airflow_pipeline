76024dadc50c0b9c982e73ef081e5ebd40d302b4
FabioCarusi <carusi.fabio@gmail.com>
2025-07-25 12:46:58 +0200

    query

---
+++ include/query.py
@@ -0,0 +1,28 @@
+import sqlite3
+import os
+from store_news import get_db_path
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_DIR = os.path.join(PROJECT_ROOT, "data")
+DB_NAME = 'news_feed.db'
+
+db_path = get_db_path(DATA_DIR, DB_NAME)
+print(db_path)
+
+try:
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        
+        truncate = cursor.execute("""
+                       DELETE FROM articles
+                       """)
+        conn.commit()
+        
+        query = cursor.execute("""
+                       SELECT * FROM articles
+                       """)
+        
+        print(query.fetchall())
+        conn.close()
+except sqlite3.Error as e:
+        print(e)
